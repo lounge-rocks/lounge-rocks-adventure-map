b9fa7b90029a63ffde6db9a39e046b7923c18f45
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-16 00:01:15 +0100

    Tiled project (#3)
    * use terrain tool to create lake
    
    * add castle
    
    * move blocking back down
    
    * add bridges
    
    * fix some layers
    
    * remove tiled-session

---
+++ !fire.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="!fire" tilewidth="32" tileheight="32" tilecount="192" columns="12">
+ <image source="opengameapi/LPC/Characters/!fire.png" width="384" height="512"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
